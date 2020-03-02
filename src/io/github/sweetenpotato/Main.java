996927e6c7e1d96fe4524eb6ccb6c66559f5b034
sweetenpotato <rayme18@naver.com>
2020-03-02 18:28:29 +0900

    Command spawn modified
    Command spawn modified

--- src/io/github/sweetenpotato/Main.java
+++ src/io/github/sweetenpotato/Main.java
@@ -10,13 +10,13 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	
+
 	public final Logger logger = Logger.getLogger("Minecraft");
 	public static Main plugin;
 
 	PluginDescriptionFile pdFile = this.getDescription();
-	
-	//색깔 간략화
+
+	// 색깔 간략화
 	String white = ChatColor.WHITE + "";
 	String red = ChatColor.RED + "";
 	String dark_red = ChatColor.DARK_RED + "";
@@ -29,7 +29,7 @@ public class Main extends JavaPlugin {
 	String dark_blue = ChatColor.DARK_BLUE + "";
 	String gray = ChatColor.GRAY + "";
 
-	//플레이어 채팅 상에서
+	// 플레이어 채팅 상에서
 	String chat_prefix = aqua + "[EssentialReplace] " + "";
 	String chat_info = yellow + "[Info] " + "";
 	String chat_error = dark_red + "[Error] " + red + "";
@@ -38,7 +38,7 @@ public class Main extends JavaPlugin {
 	String chat_plvers = green + " version : " + pdFile.getVersion() + "";
 	String chat_plinfo = chat_plname + chat_plvers + "";
 
-	//콘솔 창에서
+	// 콘솔 창에서
 	String dos_prefix = "[EssentialReplace] ";
 	String dos_info = "[Info] ";
 	String dos_error = "[Error] ";
@@ -46,33 +46,39 @@ public class Main extends JavaPlugin {
 	String dos_plname = pdFile.getName() + "";
 	String dos_plvers = " version : " + pdFile.getVersion() + "";
 	String dos_plinfo = dos_plname + dos_plvers + "";
-	
+
 	@Override
 	public void onEnable() {
 		console(dos_prefix + dos_info + dos_plinfo + "가 활성화되었습니다.");
+		getCommand("spawn").setExecutor(new CommandSpawn());
 	}
-	
+
 	@Override
 	public void onDisable() {
 		console(dos_prefix + dos_info + dos_plinfo + "가 비활성화되었습니다.");
 	}
-	
+
 	public void console(String msg) {
 		getLogger().info(msg);
 	}
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("spawn")) {
+		if (label.equalsIgnoreCase("es")) {
 			if (sender instanceof Player) {
-				Player player = (Player) sender;
-				player.teleport(player.getWorld().getSpawnLocation());
-				player.sendMessage(chat_prefix + green + "스폰으로 이동하였습니다.");
+				if (args.length < 1) {
+					sender.sendMessage(
+							yellow + "------------" + green + "[EssentialReplace]" + yellow + "------------");
+					sender.sendMessage(chat_prefix + chat_info + aqua + "Plugin Name : " + pdFile.getName());
+					sender.sendMessage(chat_prefix + chat_info + aqua + "Plugin Version : " + pdFile.getVersion());
+					sender.sendMessage(chat_prefix + chat_info + aqua + "Minecraft Version : " + pdFile.getAPIVersion());
+					sender.sendMessage(chat_prefix + chat_info + aqua + "Author : " + pdFile.getAuthors());
+					sender.sendMessage(chat_prefix + chat_info + aqua + "Please type " + yellow + "/es help" + "");
+				}
+				return true;
+			} else {
+
 			}
-			else {
-				sender.sendMessage(dos_prefix + "이 명령어는 콘솔에서는 사용할 수 없는 명령어입니다.");
-			}
-			return true;
 		}
 		return false;
 	}
