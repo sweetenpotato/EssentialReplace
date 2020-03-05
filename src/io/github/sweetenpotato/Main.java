647be50b129fa795cbd0e5d00554fc848bb813b1
sweetenpotato <rayme18@naver.com>
2020-03-05 23:11:22 +0900

    es command added
    console

--- src/io/github/sweetenpotato/Main.java
+++ src/io/github/sweetenpotato/Main.java
@@ -14,9 +14,9 @@ public class Main extends JavaPlugin {
 
 	public final Logger logger = Logger.getLogger("Minecraft");
 	public static Main plugin;
-
+	
 	PluginDescriptionFile pdFile = this.getDescription();
-
+	
 	// 색깔 간략화
 	String white = ChatColor.WHITE + "";
 	String red = ChatColor.RED + "";
@@ -47,12 +47,14 @@ public class Main extends JavaPlugin {
 	String dos_plname = pdFile.getName() + "";
 	String dos_plvers = " version : " + pdFile.getVersion() + "";
 	String dos_plinfo = dos_plname + dos_plvers + "";
-	
+
 	@Override
 	public void onEnable() {
 		console(dos_prefix + dos_info + dos_plinfo + "가 활성화되었습니다.");
-		if (plugin_on == true) {
-			getCommand("spawn").setExecutor(new CommandSpawn());
+		if (plugin_on) {
+			cmd();
+		}
+		else {
 		}
 	}
 
@@ -64,61 +66,10 @@ public class Main extends JavaPlugin {
 	public void console(String msg) {
 		getLogger().info(msg);
 	}
-
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (label.equalsIgnoreCase("es")) {
-			if (sender instanceof Player) {
-				if (args.length < 1) {
-					sender.sendMessage(
-							yellow + "------------" + green + "[EssentialReplace]" + yellow + "------------");
-					sender.sendMessage(chat_prefix + chat_info + blue + "플러그인 이름 : " + pdFile.getName());
-					sender.sendMessage(chat_prefix + chat_info + blue + "플러그인 버전 : " + pdFile.getVersion());
-					sender.sendMessage(chat_prefix + chat_info + blue + "마인크래프트 버전 : " + pdFile.getAPIVersion());
-					sender.sendMessage(chat_prefix + chat_info + blue + "개발자 : " + pdFile.getAuthors());
-					sender.sendMessage(chat_prefix + chat_info + blue + "EssentialReplace의 명령어 목록을 보시려면 " + yellow
-							+ "/es help" + blue + "를 입력해 주세요.");
-				} else {
-					if (args[0].equalsIgnoreCase("help")) {
-						sender.sendMessage(
-								yellow + "------------" + green + "[EssentialReplace]" + yellow + "------------");
-						sender.sendMessage(chat_prefix + chat_info + blue + "/es reload" + green + " : 플러그인을 다시 불러옵니다." );
-						sender.sendMessage(chat_prefix + chat_info + blue + "/es off" + green + " : 명령어를 인식하지 않게 합니다.");
-						sender.sendMessage(chat_prefix + chat_info + blue + "/es on" + green + " : 명령어를 인식하게 합니다.");
-						sender.sendMessage(chat_prefix + chat_info + blue + "/es info" + green + " : EssentialReplace의 정보를 표시합니다.");
-						sender.sendMessage(chat_prefix + chat_info + blue + "/spawn" + green + " : 스폰으로 이동합니다.");
-					}
-					else {
-						sender.sendMessage(chat_prefix + chat_error + "올바른 명령어가 아닙니다. /es help를 입력하여 명령어를 확인하세요.");
-					}
-				}
-				return true;
-			} //입력한 사람이 player인 경우
-			
-			else {
-				if (args.length < 1) {
-					sender.sendMessage("------------[EssentialReplace]------------");
-					sender.sendMessage(dos_prefix + dos_info + "플러그인 이름 : " + pdFile.getName());
-					sender.sendMessage(dos_prefix + dos_info + "플러그인 버전 : " + pdFile.getVersion());
-					sender.sendMessage(dos_prefix + dos_info + "마인크래프트 버전 : " + pdFile.getAPIVersion());
-					sender.sendMessage(dos_prefix + dos_info + "개발자 : " + pdFile.getAuthors());
-					sender.sendMessage(dos_prefix + dos_info + "EssentialReplace의 명령어 목록을 보시려면 /es help를 입력해 주세요.");
-				} else {
-					if (args[0].equalsIgnoreCase("help")) {
-						sender.sendMessage("------------[EssentialReplace]------------");
-						sender.sendMessage(dos_prefix + dos_info + "/es reload : 플러그인을 다시 불러옵니다." );
-						sender.sendMessage(dos_prefix + dos_info + "/es off : 명령어를 인식하지 않게 합니다.");
-						sender.sendMessage(dos_prefix + dos_info + "/es on : 명령어를 인식하게 합니다.");
-						sender.sendMessage(dos_prefix + dos_info + "/es info : EssentialReplace의 정보를 표시합니다.");
-						sender.sendMessage(dos_prefix + dos_info + "/spawn : 스폰으로 이동합니다.");
-					}
-					else {
-						sender.sendMessage(dos_prefix + dos_error + "올바른 명령어가 아닙니다. /es help를 입력하여 명령어를 확인하세요.");
-					}
-				}
-				return true;
-			}
-		}
-		return false;
+	
+	public void cmd() {
+		getCommand("es").setExecutor(new CommandEs());
 	}
+	
+	public
 }
